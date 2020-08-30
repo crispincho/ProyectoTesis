82856e98f83ae6043566a99c9ff0939053456f82
Cristhian Rodriguez <crisdrodriguezm@hotmail.com>
2020-08-30 12:22:20 -0500

    Agregando objetos de firebase database

--- app/src/main/java/com/umb/myapplication/features/formulario/data/FormularioRepository.kt
+++ app/src/main/java/com/umb/myapplication/features/formulario/data/FormularioRepository.kt
@@ -1,5 +1,17 @@
 package com.umb.myapplication.features.formulario.data
 
+import android.content.Context
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
 object FormularioRepository {
+
+    val firebase: FirebaseDatabase = FirebaseDatabase.getInstance()
+    val databaseReference: DatabaseReference = firebase.reference
+
+    fun initFirebase(context: Context){
+        FirebaseApp.initializeApp(context)
+    }
 
 }
