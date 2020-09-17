e889df26b93dccd65dfbeeade6ea99fbe98ba74d
Harold Rodriguez <harold_r008@hotmail.com>
2020-09-17 08:38:46 -0500

    Salvando Recursos equipo

---
+++ app/src/main/java/com/umb/myapplication/features/rfn007/ui/Rfn007Activity.kt
@@ -0,0 +1,12 @@
+package com.umb.myapplication.features.rfn007.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.umb.myapplication.R
+
+class Rfn007Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rfn007)
+    }
+}
---
+++ app/src/main/java/com/umb/myapplication/features/rfn007/ui/viewmodel/Rfn007ViewModelFactory.kt
@@ -0,0 +1,5 @@
+package com.umb.myapplication.features.rfn007.ui.viewmodel
+
+class Rfn007ViewModelFactory {
+
+}
