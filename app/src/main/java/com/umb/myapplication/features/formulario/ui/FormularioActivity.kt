3d750e0a9ab190ebd01970a88bbcc3a60c0f5841
Cristhian Rodriguez <crisdrodriguezm@hotmail.com>
2020-10-04 19:27:18 -0500

    Solicitando permiso para microfono

--- app/src/main/java/com/umb/myapplication/features/formulario/ui/FormularioActivity.kt
+++ app/src/main/java/com/umb/myapplication/features/formulario/ui/FormularioActivity.kt
@@ -1,11 +1,14 @@
 package com.umb.myapplication.features.formulario.ui
 
 import android.content.Intent
-import android.media.MediaPlayer
+import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.umb.myapplication.R
@@ -15,6 +18,9 @@ import com.umb.myapplication.features.formulario.ui.viewmodel.FormularioViewmode
 import com.umb.myapplication.features.rfn004.ui.Rfn004Activity
 
 class FormularioActivity : AppCompatActivity(), FormularioNavigator {
+
+    private val REQUEST_RECOD_AUDIO = 1001
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityFormularioBinding>(
@@ -26,7 +32,9 @@ class FormularioActivity : AppCompatActivity(), FormularioNavigator {
         viewmodel.navigator = this
         binding.viewmodel = viewmodel
         binding.lifecycleOwner = this
-        toNextActvity("")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            validatePermission()
+        }
     }
 
     @Suppress("DEPRECATION")
@@ -41,10 +49,52 @@ class FormularioActivity : AppCompatActivity(), FormularioNavigator {
     }
 
     override fun toNextActvity(idUser: String) {
-        val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.rfn005_instrucciones)
-        mediaPlayer?.start()
-//        val intent = Intent(this, Rfn004Activity::class.java)
-//        intent.putExtra("idUser", idUser)
-//        startActivity(intent)
+        val intent = Intent(this, Rfn004Activity::class.java)
+        intent.putExtra("idUser", idUser)
+        startActivity(intent)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun validatePermission() {
+        if (checkSelfPermission(android.Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+            Log.i("Prueba", "Tiene permiso")
+        } else {
+            if (shouldShowRequestPermissionRationale(android.Manifest.permission.RECORD_AUDIO)) {
+                Toast.makeText(
+                    this,
+                    "Permiso del microfono es necesarioa para el uso de la aplicacion",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            requestPermissions(
+                arrayOf(android.Manifest.permission.RECORD_AUDIO),
+                REQUEST_RECOD_AUDIO
+            )
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_RECOD_AUDIO) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.i("Prueba", "Tiene permiso")
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permiso del microfono es necesarioa para el uso de la aplicacion",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            requestPermissions(
+                arrayOf(android.Manifest.permission.RECORD_AUDIO),
+                REQUEST_RECOD_AUDIO
+            )
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
     }
 }
