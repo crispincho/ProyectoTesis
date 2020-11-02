c166cabb304bf878426881eeb3e76e922574b4e0
Harold Rodriguez <harold_r008@hotmail.com>
2020-11-01 19:34:15 -0500

    prueba Rfn009

--- app/src/main/java/com/umb/myapplication/features/rfn009/ui/Rfn009Activity.kt
+++ app/src/main/java/com/umb/myapplication/features/rfn009/ui/Rfn009Activity.kt
@@ -1,14 +1,20 @@
 package com.umb.myapplication.features.rfn009.ui
 
+import android.app.PendingIntent.getActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.umb.myapplication.R
 import com.umb.myapplication.core.ui.GameActivity
+import com.umb.myapplication.core.utils.DialogInstructions
+import com.umb.myapplication.core.utils.DialogInstructionsListener
 import com.umb.myapplication.databinding.ActivityRfn009Binding
 import com.umb.myapplication.features.rfn009.ui.viewmodel.Rfn009ViewModel
 import com.umb.myapplication.features.rfn009.ui.viewmodel.Rfn009ViewModelFactory
+import kotlinx.android.synthetic.main.status_bar.view.*
 
 class Rfn009Activity : GameActivity(), Rfn009Navigator {
 
@@ -22,18 +28,43 @@ class Rfn009Activity : GameActivity(), Rfn009Navigator {
         viewmodel.navigator = this
         viewmodel.idUser = intent.getStringExtra(Companion.ID_USER) ?: ""
         viewmodel.score.observe(this, Observer {
-            // TODO: 4/10/2020 need do a link with the layout view
+            binding.statusBar.tvPoints.text = it.toString()
         })
         binding.viewmodel = viewmodel
+        binding.workCorrect.visibility = View.INVISIBLE
         binding.lifecycleOwner = this
         activateView()
     }
 
     private fun activateView() {
+        val dialog = DialogInstructions(
+            this,
+            getString(R.string.test_rfn009_instructions),
+            R.raw.rfn008_instrucciones,
+            object : DialogInstructionsListener {
+                override fun dismmissDialog() {
+                    binding.statusBar.tvTime.base = SystemClock.elapsedRealtime()
+                    binding.statusBar.tvTime.start()
 
+                    binding.viewmodel!!.initWork(binding.work)
+                }
+            })
+        dialog.show()
     }
 
     override fun toNextActvity() {
-        TODO("Not yet implemented")
+        finish()
+        System.exit(0)
     }
+
+    override fun activateTest() {
+        binding.statusBar.tvTime.base = SystemClock.elapsedRealtime()
+        binding.statusBar.tvTime.start()
+    }
+
+    override fun getUserID(): String {
+        return intent.getStringExtra(ID_USER)!!
+    }
+
+
 }
--- app/src/main/java/com/umb/myapplication/features/rfn009/ui/Rfn009Navigator.kt
+++ app/src/main/java/com/umb/myapplication/features/rfn009/ui/Rfn009Navigator.kt
@@ -3,5 +3,7 @@ package com.umb.myapplication.features.rfn009.ui
 interface Rfn009Navigator {
 
     fun toNextActvity()
+    fun activateTest()
+    fun getUserID():String
 
 }
